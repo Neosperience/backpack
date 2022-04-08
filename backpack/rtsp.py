fd2fdb2c704c0a2e7cd1c1326979407738cb1d41
Janos Tolgyesi <janos.tolgyesi@neosperience.com>
2022-04-08 17:48:51 +0200

    Multi-stream RTSP server actually works

--- backpack/rtsp.py
+++ backpack/rtsp.py
@@ -1,28 +1,34 @@
-from typing import Any
+from typing import Any, Optional
 import multiprocessing
+import threading
+import logging
 
 import gi
 
 gi.require_version('Gst', '1.0')
 gi.require_version('GstRtspServer', '1.0')
-from gi.repository import GLib, Gst, GstRtspServer
+from gi.repository import GLib, GstRtspServer
 
 from .spyglass import SpyGlass
 
-class RTSPServer(multiprocessing.Process):
+class RTSPServer:
 
     # TODO: this should be a singleton among all RTSPSpyGlasses
 
-    def __init__(self, port: str) -> None:
-        super().__init__(name='RTSPSpyGlassServerProcess')
+    def __init__(self, port: str, parent_logger: Optional[logging.Logger] = None,) -> None:
+        self.logger = (
+            logging.getLogger(self.__class__.__name__) if parent_logger is None else
+            parent_logger.getChild(self.__class__.__name__)
+        )
         self._port = port
         self.gst_server = GstRtspServer.RTSPServer()
         self.gst_server.service = port
         self.streams = {}
-        self.gst_server.attach()
         self.loop = None
+        self.process = None
 
     def add_stream(self, mount_point: str, pipeline: str) -> None:
+        self.logger.info('Adding pipeline to mount point "%s"', mount_point)
         mounts = self.gst_server.get_mount_points()
         factory = GstRtspServer.RTSPMediaFactory()
         factory.set_launch(pipeline)
@@ -35,9 +41,20 @@ class RTSPServer(multiprocessing.Process):
         mounts.remove_factory(mount_point)
         del self.streams[mount_point]
 
-    def run(self):
-        self.loop = GLib.MainLoop()
-        self.loop.run()
+    def start(self):
+        self.logger.info('Starting RTSPServer')
+        self.gst_server.attach()
+        self.thread = threading.Thread(
+            name='RTSPServerThread', 
+            target=GLib.MainLoop().run
+        )
+        self.thread.start()
+        # self.process = multiprocessing.Process(
+        #     name='RTSPServerProcess', 
+        #     target=GLib.MainLoop().run, 
+        #     daemon=True
+        # )
+        # self.process.start()
 
     @property
     def port(self) -> str:
@@ -54,15 +71,16 @@ class RTSPSpyGlass(SpyGlass):
     LOCALHOST = '127.0.0.1'
     last_loopback_port = 5000
 
-    def __init__(self, port: str, path: str, *args: Any, **kwargs: Any) -> None:
+    def __init__(self, server: RTSPServer, path: str, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
-        self.server = RTSPServer(port)
+        self.server = server
         self.path = path
         self.loopback_port = RTSPSpyGlass.last_loopback_port
         RTSPSpyGlass.last_loopback_port += 1
+        self.server.add_stream(self.path, self._get_server_pipeline())
 
     def _get_pipeline(self, fps: float, width: int, height: int) -> str:
-        return ' ! '.join([
+        pipeline = ' ! '.join([
             'appsrc',
             'queue',
             'videoconvert',
@@ -85,11 +103,3 @@ class RTSPSpyGlass(SpyGlass):
         ])
         self.logger.info(f'GStreamer RTSP server pipeline definition:\n{pipeline}')
         return pipeline
-
-    def _try_open_stream(self, fps, width, height):
-        success = super()._try_open_stream(fps, width, height)
-        if success:
-            server_pipeline = self._get_server_pipeline()
-            self.server.add_stream(self.path, server_pipeline)
-            self.server.start()
-        return success
