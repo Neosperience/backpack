b3581a74a308310bdb27eced1a58553682122ed4
Janos Tolgyesi <janos.tolgyesi@neosperience.com>
2022-04-11 18:36:52 +0200

    Add RTSP Spyglass docstrings

--- backpack/rtsp.py
+++ backpack/rtsp.py
@@ -1,5 +1,17 @@
-from typing import Any, Optional
-import multiprocessing
+'''
+This module contains RTSPSpyGlass and RTSPServer. These two classes allow serving a sequence of
+OpenCV images as video streams using the RTSP protocol.
+
+To use this class you MUST have the following dependencies correctly configured on your system:
+ - GStreamer 1.0 installed with standard plugins pack, libav, tools and development libraries
+ - OpenCV 4.2.0, compiled with GStreamer support and Python bindings
+ - gst-rtsp-server with development libraries (libgstrtspserver-1.0-dev)
+
+These dependencies can not be easily specified by a requirements.txt or a Conda environment.
+See the example Dockerfile on how to install these dependencies on your system.
+'''
+
+from typing import Any, Optional, List
 import threading
 import logging
 
@@ -12,22 +24,50 @@ from gi.repository import GLib, GstRtspServer
 from .spyglass import SpyGlass
 
 class RTSPServer:
+    ''' The RTSPServer instance wraps a GStreamer RTSP server that serves video streams to clients
+    using the RTSP protocol.
 
-    # TODO: this should be a singleton among all RTSPSpyGlasses
+    You typically want to have a single instance of RTSPServer for your application. You can 
+    register any number of video streams that will be served by a single instance of RTSP server.
+    The port number of the server should be unique among all applications on the device.
 
-    def __init__(self, port: str, parent_logger: Optional[logging.Logger] = None,) -> None:
+    For an example usage of RTSPServer, see the documentation of RTSPSpyGlass class.
+    
+    :param port: The port to listen on. You can not modify the port after the initialization of
+        the RTSPServer instance. Defaults to "8554".
+    :param parent_logger: If you want to connect the logger of th RTSPServer to a parent,
+        specify it here.
+    '''
+
+    def __init__(
+        self, 
+        port: str = '8554', 
+        parent_logger: Optional[logging.Logger] = None,
+    ) -> None:
         self.logger = (
             logging.getLogger(self.__class__.__name__) if parent_logger is None else
             parent_logger.getChild(self.__class__.__name__)
         )
-        self._port = port
         self.gst_server = GstRtspServer.RTSPServer()
         self.gst_server.service = port
         self.streams = {}
-        self.loop = None
-        self.process = None
+        self._port = port
+        self._loop = GLib.MainLoop()
+        self._thread = None
 
     def add_stream(self, mount_point: str, pipeline: str) -> None:
+        ''' Registers a new video stream to the server. 
+        
+        :param mount_point: The path that will be used to access the stream. For example, 
+            if you specify "/my_stream", the stream will be accessible for clients using
+            the "rtsp://127.0.0.1:8854/mystream" url (change the IP address and the port
+            number accordingly).
+        :param pipeline: The GStreamer pipeline to use for the stream. This will be typically
+            a pipeline picking up the raw UDP packets from a local port and wrapping it to a
+            H.264 envelope, for example: 
+            "udpsrc port=5000 ! application/x-rtp,media=video,encoding-name=H264 ! rtph264depay !
+            rtph264pay name=pay0"
+        '''
         self.logger.info('Adding pipeline to mount point "%s"', mount_point)
         mounts = self.gst_server.get_mount_points()
         factory = GstRtspServer.RTSPMediaFactory()
@@ -37,36 +77,85 @@ class RTSPServer:
         self.streams[mount_point] = pipeline
 
     def remove_stream(self, mount_point: str) -> None:
+        ''' It removes a registered stream from the server.
+        
+        :param mount_point: The registered path of the stream.
+        '''
         mounts = self.gst_server.get_mount_points()
         mounts.remove_factory(mount_point)
         del self.streams[mount_point]
 
     def start(self):
+        ''' Starts the RTSP server asynchronously. 
+        
+        After calling this method, the RTSP requests will be served.
+        '''
         self.logger.info('Starting RTSPServer')
         self.gst_server.attach()
-        self.thread = threading.Thread(
+        self._thread = threading.Thread(
             name='RTSPServerThread', 
-            target=GLib.MainLoop().run
+            target=self._loop.run,
+            daemon=True
         )
-        self.thread.start()
-        # self.process = multiprocessing.Process(
-        #     name='RTSPServerProcess', 
-        #     target=GLib.MainLoop().run, 
-        #     daemon=True
-        # )
-        # self.process.start()
+        self._thread.start()
+    
+    def stop(self):
+        ''' Stops the RTSP server. 
+        
+        After calling this method, no RTSP requests will be served. The server can be restarted
+        later on calling the start() method.
+        '''
+        self._loop.quit()
 
     @property
     def port(self) -> str:
+        ''' The port where this server listents to incoming connections. '''
         return self._port
 
+    def urls(self) -> List[str]:
+        ''' Returns the list of URLs where the server serves RTSP streams. '''
+        return [f'rtsp://127.0.0.1:{self.port}{mp}' for mp in self.streams.keys()]
+
     def __repr__(self) -> str:
-        streams_repr = [f'rtsp://127.0.0.1:{self.port}{mp}' for mp in self.streams.keys()]
-        return f'<{self.__class__.__name__} streams=[{", ".join(streams_repr)}]>'
+        return f'<{self.__class__.__name__} streams=[{", ".join(self.urls())}]>'
 
 
 
 class RTSPSpyGlass(SpyGlass):
+    ''' Together with RTSPServer, RTSPSpyGlass a sequence of OpenCV frames on the RTSP protocol.
+
+    A single instance of RTSPServer application can serve streams coming from multiple RTSPSpyGlass
+    instances. You should instantiate the RTSPServer instance first. For example, if you want to
+    serve two seperate RTSP streams, you could use this code to set up your scenario:
+
+    ```python
+    server = RTSPServer(port="8554")
+    spyglass1 = RTSPSpyGlass(server, "/stream1")
+    spyglass2 = RTSPSpyGlass(server, "/stream2")
+    spyglass1.start_streaming(30, 640, 480)
+    spyglass2.start_streaming(30, 640, 480)
+    server.start()
+
+    while True:
+        frame1 = ... # Get frame for the first stream as a numpy array of shape (640, 480, 3)
+        frame2 = ... # Get frame for the second stream
+        spyglass1.put(frame1)
+        spyglass2.put(frame2)
+    ```
+
+    Using this code, you can access the streams at the following URLs:
+        - rtsp://127.0.0.1:8554/stream1
+        - rtsp://127.0.0.1:8554/stream2
+
+    If the application (or the firewall) is configured to allow incoming connections on the 8554
+    port, the streams will be accessibly also from the external ip of the device.
+
+    :param server: The RTSPServer instance that this stream is being served by
+    :param path: The path to the stream. This is the path that the client will use to connect to 
+        the stream
+    :param args: Positional arguments to be passed to SpyGlass superclass initializer.
+    :param kwargs: Keyword arguments to be passed to SpyGlass superclass initializer.
+    '''
 
     LOCALHOST = '127.0.0.1'
     last_loopback_port = 5000
@@ -74,7 +163,7 @@ class RTSPSpyGlass(SpyGlass):
     def __init__(self, server: RTSPServer, path: str, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
         self.server = server
-        self.path = path
+        self.path = path if path.startswith('/') else '/' + path
         self.loopback_port = RTSPSpyGlass.last_loopback_port
         RTSPSpyGlass.last_loopback_port += 1
         self.server.add_stream(self.path, self._get_server_pipeline())
---
+++ tests/rtspservertest.py
@@ -0,0 +1,31 @@
+import backpack.rtsp, gi, numpy as np, time, os, logging
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst
+Gst.init()
+os.environ['GST_DEBUG'] = '3'
+logging.basicConfig(level=logging.INFO)
+
+server = backpack.rtsp.RTSPServer(port='8554')
+sg1 = backpack.rtsp.RTSPSpyGlass(server, 'rainbow')
+sg2 = backpack.rtsp.RTSPSpyGlass(server, 'bw')
+sg1.start_streaming(30, 640, 480)
+sg2.start_streaming(30, 640, 480)
+server.start()
+
+# Generate some fake videos
+import colorsys
+from backpack.annotation import TimestampAnnotation, OpenCVImageAnnotationDriver
+i = 0
+driver = OpenCVImageAnnotationDriver()
+while True:
+    i = (i + 1) % 256
+    (r, g, b) = colorsys.hsv_to_rgb(255 - i / 255, 1.0, 1.0)
+    R, G, B = int(255 * r), int(255 * g), int(255 * b)
+    annos = [TimestampAnnotation()]
+    frame1 = np.full((640, 480, 3), [R, G, B], dtype=np.uint8)
+    _ = driver.render(annos, frame1)
+    frame2 = np.full((640, 480, 3), [i, i, i], dtype=np.uint8)
+    _ = driver.render(annos, frame2)
+    _ = sg1.put(frame1)
+    _ = sg2.put(frame2)
+    time.sleep(1/30)
