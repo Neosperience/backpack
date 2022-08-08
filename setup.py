b631da40700d556d622101da91de3e106b32a5f5
Janos Tolgyesi <janos.tolgyesi@neosperience.com>
2022-08-08 17:44:01 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,23 +4,27 @@ import os
 from setuptools import setup
 
 name = "panorama-backpack"
-here = os.path.abspath(os.path.dirname(__file__))
+version = "v0.1.1"
 
 # get the dependencies and installs
-with open('requirements.txt', 'r') as f:
+with open("requirements.txt", "r") as f:
     requires = [x.strip() for x in f if x.strip()]
 
 setup(
     name=name,
-    version="0.1",
+    version=version,
     description="Tools for AWS Panorama development",
     python_requires=">=3.7",
-    packages=['backpack'],
-    package_dir={'backpack': 'backpack'},
-    py_modules=[os.path.splitext(os.path.basename(path))[0] for path in glob('src/*.py')],
+    packages=["backpack"],
+    package_dir={"backpack": "backpack"},
+    py_modules=[os.path.splitext(os.path.basename(path))[0] for path in glob("src/*.py")],
     include_package_data=True,
     install_requires=requires,
-    author="Neosperience",
+    author="Janos Tolgyesi",
+    author_email='janos.tolgyesi@neosperience.com',
+    url="https://github.com/Neosperience/backpack",
+    download_url="https://github.com/Neosperience/backpack/archive/refs/tags/{}.tar.gz".format(version),
+    keywords=["aws", "panorama", "video-analysis"],
     entry_points={},
     zip_safe=False,
     classifiers=[
