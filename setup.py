d81970b84105b91a12a89608e6ce299eee93fbe9
Janos Tolgyesi <janos.tolgyesi@neosperience.com>
2022-08-10 18:10:17 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 from setuptools import setup, find_packages
 
 name = "panorama-backpack"
-version = "0.1.2"
+version = "0.1.3"
 
 # get the dependencies and installs
 with open("requirements.txt", "r") as f:
