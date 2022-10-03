02c90717eaef2e9008417a1a8fc23f0ecf83dd07
Janos Tolgyesi <janos.tolgyesi@neosperience.com>
2022-10-03 16:29:27 +0200

    Single-source package version

--- backpack/__init__.py
+++ backpack/__init__.py
@@ -1,5 +1,7 @@
 ''' Utilities for AWS Panorama application development. '''
 
+__version__ = '0.1.6'
+
 import functools
 
 def lazy_property(func):
--- setup.py
+++ setup.py
@@ -1,17 +1,31 @@
-from glob import glob
 import os
+import codecs
+import os.path
 
 from setuptools import setup, find_packages
 
 name = "panorama-backpack"
-version = "0.1.6"
 
-# get the dependencies and installs
 with open("requirements.txt", "r") as f:
     requires = [x.strip() for x in f if x.strip()]
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+version=get_version("backpack/__init__.py")
 
 setup(
     name=name,
