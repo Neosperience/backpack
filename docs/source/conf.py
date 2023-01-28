6e9f8b023039abc481caa91ede5daa10f69ccf70
Janos Tolgyesi <janos.tolgyesi@neosperience.com>
2023-01-28 11:06:41 +0100

    Fix version in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,17 +12,32 @@
 #
 import os
 import sys
+import codecs
 sys.path.insert(0, os.path.abspath('../..'))
 
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+version = get_version("../../backpack/__init__.py")
 
 # -- Project information -----------------------------------------------------
 
 project = 'backpack'
-copyright = '2022, Janos Tolgyesi'
+copyright = '2023, Janos Tolgyesi'
 author = 'Janos Tolgyesi'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.1.6'
+release = 'v' + version
 
 
 # -- General configuration ---------------------------------------------------
